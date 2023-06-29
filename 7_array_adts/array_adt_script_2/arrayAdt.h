d5bb238f1c3848dff3ef96bc5f79a9cbd29b7a14
Jimmy Bates <jbates.productions@gmail.com>
2023-06-28 21:35:58 -0700

    Updated for some better ADT settings

---
+++ 7_array_adts/array_adt_script_2/arrayAdt.h
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define RIGHT   1
+#define LEFT    0
+
+struct ArrayInt {
+    int* A;
+    int length;
+    int size;
+};
+
+int initADT(struct ArrayInt *arr, int* val, int size, int length);
+int freeADT(struct ArrayInt *arr);
+
+void swap(void *x, void *y, size_t size);
+
+int displayADT(struct ArrayInt *arr);
+int appendADT(struct ArrayInt *arr, int val);
+int insertADT(struct ArrayInt *arr, int val, int pos);
+int deleteADT(struct ArrayInt *arr, int pos);
+int linSearchADT(struct ArrayInt *arr, int val);
+int binSearchADT(struct ArrayInt *arr, int val, int lo, int hi);
+int dncSortADT(struct ArrayInt *arr, int lo, int hi);
+int getADT(struct ArrayInt *arr, int pos);
+int setADT(struct ArrayInt *arr, int val, int pos);
+int maxADT(struct ArrayInt *arr);
+int minADT(struct ArrayInt *arr);
+int reverseADT(struct ArrayInt *arr);
+int shiftADT(struct ArrayInt *arr, int n, int dir);
+int rotateADT(struct ArrayInt *arr, int n, int dir);
