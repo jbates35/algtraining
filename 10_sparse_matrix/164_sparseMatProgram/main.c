ef0693e8ecae1a05c23a3e411a6f6c6ca5c9e2c4
Jimmy Bates <jbates.productions@gmail.com>
2023-07-15 22:54:27 -0700

    Setup skeleton for main program

--- 10_sparse_matrix/164_sparseMatProgram/main.c
+++ 10_sparse_matrix/164_sparseMatProgram/main.c
@@ -6,7 +6,37 @@
 
 #include "sparseMatrix.h"
 
+void enterMats(void);
+void addMats(void);
+
 int main(int argc, char *argv[])
 {
+    void (*fps[2])(void) = {
+        &enterMats,
+        &addMats};
+
+    int option = -1;
+
+    while (option != 0)
+    {
+        printf("\n\nSelect options, or enter 0 to exit:");
+        printf("\n\t(1) Enter one matrix and sort it");
+        printf("\n\t(2) Add two matrices\n");
+
+        scanf("%d", &option);
+
+        // Execute function based on argument
+        if (option != 0)
+            fps[option - 1]();
+    }
+
     return 0;
+}
+
+void enterMats(void)
+{
+}
+
+void addMats(void)
+{
 }
