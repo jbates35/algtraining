bdec06ea6e1d16f07d3732e812a8f5bb9ef3877c
Jimmy Bates <jbates.productions@gmail.com>
2023-11-06 08:51:20 -0800

    starting framework laid out for heap sort

--- 19_heap/include/heap.h
+++ 19_heap/include/heap.h
@@ -1,2 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+void heap_insert(int A[], int n);
+void heap_delete(int A[], int m);
+
+void heap_create(int A[]);
+void heap_sort(int A[]);
--- 19_heap/src/heap.c
+++ 19_heap/src/heap.c
@@ -1,1 +1,28 @@
 #include "heap.h"
+#include <memory.h>
+
+void swap(void *x, void *y, size_t varSize);
+int isMarsenne(int x);
+
+// From perspective of heapify
+void heap_insert(int A[], int n) {}
+void heap_delete(int A[], int m) {}
+void heap_create(int A[]) {}
+void heap_sort(int A[]) {}
+
+void swap(void *x, void *y, size_t varSize) {
+  void *temp = malloc(varSize);
+  memcpy(temp, y, varSize);
+  memcpy(y, x, varSize);
+  memcpy(x, temp, varSize);
+  free(temp);
+}
+
+int isMarsenne(int x) {
+  while (x % 2 == 1)
+    x = x >> 1;
+
+  if (x == 0)
+    return 1;
+  return 0;
+}
