086c46cbbfc15332a27e2e97d4968042add5a598
Jimmy Bates <jbates.productions@gmail.com>
2023-11-14 18:29:33 -0800

    chaining started

--- 21_hashing/include/hashing.h
+++ 21_hashing/include/hashing.h
@@ -5,13 +5,16 @@ typedef struct Hashnode {
   struct Hashnode *next;
   int key;
   int val;
+  int cleared;
 } Hashnode;
 
 typedef struct Chainhash {
-  Hashnode *map;
+  Hashnode *map[16];
   int size;
 } Chainhash;
 
-void chain_init(Chainhash *map);
+void chain_init(Chainhash *hashmap);
+void chain_free(Chainhash *hashmap);
+void chain_add(Chainhash *hashmap, int key, int val);
 
 #endif
