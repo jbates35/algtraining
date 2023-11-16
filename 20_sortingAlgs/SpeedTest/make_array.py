16fcfa4c4ef16aa12bdc6c9b844a7d2fbaee23e1
Jimmy Bates <jbates.productions@gmail.com>
2023-11-15 19:18:18 -0800

    Deleted generatedarray.h

--- 20_sortingAlgs/SpeedTest/make_array.py
+++ 20_sortingAlgs/SpeedTest/make_array.py
@@ -1,7 +1,7 @@
 import random
 
 if __name__ =="__main__":
-    rand_list = [random.randint(1, 1_000_000) for _ in range(50000)]
+    rand_list = [random.randint(1, 1_000_000) for _ in range(500000)]
     with open('generatedarray.h', 'w') as f:
         f.write('int genArr[] = {')
         for i in range(len(rand_list)):
